b3180e87dcd4c81ca4901629efea059d8c2bf0b8
Adebiyidaris <adebiyidaris@rocketmail.com>
2023-02-16 16:00:21 -0800

    My first c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+/**
+ * main - Entry point
+ * put message
+ * Rrturn: Always 0 (Sucess)
+ */
+Int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle")
+	return (0);
+}
