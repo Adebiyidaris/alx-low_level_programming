98c529e04376a607bce506d72476857a5856f236
Adebiyidaris <adebiyidaris@rocketmail.com>
2023-02-16 16:30:58 -0800

    My first C program

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,11 @@
 
 /**
  * Main - Entry point
- * print  message
+ * print message
  * Return: 0
  */
 int main(void)
 {
-printf("\"with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
