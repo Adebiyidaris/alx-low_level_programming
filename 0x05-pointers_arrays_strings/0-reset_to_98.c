2dead8ebdf9ac32ce567c45e514f9904e8b954e6
Adebiyidaris <adebiyidaris@rocketmail.com>
2023-02-28 11:20:01 -0800

    my commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -9,15 +9,4 @@
 void reset_to_98(int *n)
 {
 	*n = 98;
-}#include "main.h"
-
-/**
- * reset_to_98 - updade the value of n to 98
- * @n: input
- * Return: n
- */
-
-void reset_to_98(int *n)
-{
-	*n = 98;
 }
