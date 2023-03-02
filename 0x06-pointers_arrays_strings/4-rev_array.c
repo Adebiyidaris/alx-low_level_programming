658c93813185535347dad8e1fcaf7202aabee6c1
Adebiyidaris <adebiyidaris@rocketmail.com>
2023-03-02 14:47:09 -0800

    my commit

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,9 +1,8 @@
-#include <stdio.h>
 #include "main.h"
 /**
- * _strcat - function that print a string overwriting a string
- * @src: int
- * @dest: int
+ * reverse_array - Reverses the content of an array of integers.
+ * @a: The array of integers to be reversed.
+ * @n: The number of elements in the array.
  *
  */
 void reverse_array(int *a, int n)
